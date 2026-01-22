ed45d51e7e3d7801ba28a25900e50e6590ec83ec
AYUSH AWCHAR <ayushawchar9403@gmail.com>
2026-01-22 15:43:16 +0530

    Initial setup with posts controller and routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+// src/index.js
+
+const express = require('express');
+const app = express();
+
+app.use('/api/v1/posts', require('./routes/posts.routes'));
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
